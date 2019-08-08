3f0719a38bb634b7acc02041fdc401bb8d22b8f3
josecaos <jazzvoon@gmail.com>
2019-08-08 16:09:23 -0500

    Update

--- parts/header/part-modules.php
+++ parts/header/part-modules.php
@@ -4,11 +4,11 @@
 		<?php MintModules::text(); ?>
 		<?php MintModules::icon_social(); ?>
 		<?php MintModules::icon_lwa(); ?>
-		<?php MintModules::icon_wishlist(); ?>
-		<?php MintModules::icon_minicart(); ?>
-		<?php MintModules::icon_search(); ?>
-		<?php MintModules::icon_currency(); ?>
-		<?php MintModules::icon_language_flags(); ?>
+		<?php //MintModules::icon_wishlist(); ?>
+		<?php //MintModules::icon_minicart(); ?>
+		<?php //MintModules::icon_search(); ?>
+		<?php //MintModules::icon_currency(); ?>
+		<?php //MintModules::icon_language_flags(); ?>
 
 		<?php if (MintOptions::get('header--mobile_menu')) { ?>
 			<?php MintModules::icon_mobile_menu(); ?>
